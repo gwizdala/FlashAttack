0c19f3cbaf355998d65c3c37569786ce8cdd23d2
davsuss <davsuss@vt.edu>
2015-11-07 16:11:30 -0500

    Model Example
    Much Wow, so Hack

---
+++ Web/model.js
@@ -0,0 +1,25 @@
+
+
+var model = function() {
+  //Privates
+  this.cardIndex = 0;
+  this.lives = 3;
+  this.cards = [{Question: "What is the first 3 Digits of PI?" Answer:"3.14"},
+                {Question: "What day is today?" Answer:"Saturday"}
+                ];
+}
+
+//Publics
+model.prototype.getCard = function() {
+  var card = this.cards[this.cardIndex];
+  this.cardIndex += 1;
+  return card;
+};
+
+model.prototype.getLives = function() {
+  return this.lives;
+};
+
+model.prototype.getCardsRemaining = function() {
+  return (this.cardIndex == this.cards.length);
+};
